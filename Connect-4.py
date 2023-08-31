445a5f44ff39c259a2a79e513391e57c5e37678d
xkicken <61647663+xkicken@users.noreply.github.com>
2023-08-31 15:11:03 +1200

    optimisted the code

--- Connect-4.py
+++ Connect-4.py
@@ -1,22 +1,10 @@
 def create_board(width, height):
-    count = 0
-    broad_width = ""
-    broad = []
-    if width < 4:
-        width = 4
-    elif width > 10:
-        width = 10
-    if height < 4:
-        height =4
-    elif height > 10:
-        height = 10
-    while count != width:
-        count += 1
-        broad_width = broad_width + "."
-    count = 0
-    while count != height:
-        count += 1
-        broad.append(broad_width)
-    return broad
+    width = max(4, min(width, 10))
+    height = max(4, min(height, 10))
+    
+    row = "." * width
+    board = [row for _ in range(height)]
+    
+    return board
 b = create_board(20, 10)
 print(b)
